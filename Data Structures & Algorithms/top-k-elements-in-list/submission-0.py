c4569dc7ad7bf07c4b414bac69bee781942137cd
Victor Braga <40031882+victorpazb@users.noreply.github.com>
2026-04-18 13:55:09 -0300

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        myMap = {}
+        for index, num in enumerate(nums):
+            myMap.setdefault(num, 0)
+            myMap[num] += 1
+        
+        ordenado = sorted(myMap, key=lambda x: myMap[x], reverse=True)
+        return ordenado[:k]
+
+        
